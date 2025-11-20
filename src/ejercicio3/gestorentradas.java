a0a98fa10d17f8170c9595074772835534a47b4c
ibnotis013 <n.ledo@liceolapaz.net>
2025-11-20 13:10:05 +0100

    ejercicio3 a medias

--- src/ejercicio3/gestorentradas.java
+++ src/ejercicio3/gestorentradas.java
@@ -2,6 +2,27 @@ package ejercicio3;
 
 public class gestorentradas {
 
+public static double precio(){
+return 10;
+}
+public static double preciofinal(int edad, boolean esDiaEspectador){
+    if(esDiaEspectador){
+        return precio()%20;
+    }
+    else if (edad>65){
+        return precio()%50;
+    }
+    else{
+        return precio();
+    }
+}
+public static void  imprimir(String pelicula,int butaca, double precio){
+    System.out.println("---------------------------------");
+    System.out.println(pelicula);
+    System.out.println(butaca);
+    System.out.println(precio);
+    System.out.println("---------------------------------");
+}
 
-    
+
 }
--- src/ejercicio3/tanquilla.java
+++ src/ejercicio3/tanquilla.java
@@ -1,7 +1,19 @@
 package ejercicio3;
 
+import java.util.Scanner;
+
 public class tanquilla {
     static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("introduce una peli");
+        String peli=sc.nextLine();
+        System.out.println("introduce tu edad");
+        int edad=sc.nextInt();
+        if(edad.)
+        System.out.println("es el dia del espectador S/N ?");
+        boolean diaespectador=sc.nextBoolean();
+        if(diaespectador==true){
 
+        }
     }
 }
