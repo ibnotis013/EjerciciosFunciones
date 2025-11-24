5b87efc6d6b135372df552f8b8a4e8a50da03c21
ibnotis013 <n.ledo@liceolapaz.net>
2025-11-24 12:29:34 +0100

    ejercicio1 corregido

--- src/ejercicio1/pincipal2.java
+++ src/ejercicio1/pincipal2.java
@@ -3,23 +3,19 @@ package ejercicio1;
 public class pincipal2 {
 
 
-
+    private static final double DOLAR_VALOR=1.5;
+    private static final double EURO_VALOR=0.9;
     public static void MostrarTasasDeCambio(){
-        System.out.println("la tasa de cambio es 1 credito=1.5 dolares y 1 credito=0.9€");
+        System.out.println("la tasa de cambio es 1 credito="+DOLAR_VALOR+" y 1 credito="+EURO_VALOR);
     }
     public static double dolaresacreditos(double dolares){
-        return dolares/1.5;
+        return dolares/DOLAR_VALOR;
     }
     public static double eurosacreditos(double euros){
-        return euros/0.9;
+        return euros/EURO_VALOR;
     }
     public static boolean validarcompra(double precio, double saldo){
-    if (precio > saldo){
-        return false;
-    }
-    else {
-        return true;
-    }
+    return   precio>=saldo;
     }
 
 }
--- src/ejercicio1/principal.java
+++ src/ejercicio1/principal.java
@@ -11,19 +11,17 @@ public class principal {
         System.out.println("ingrese las pelas");
         double euros = sc.nextDouble();
         double  creditos= pincipal2.eurosacreditos(euros);
-        double saldo=creditos;
         double precio=500;
-        System.out.println(creditos);
         System.out.println("intentas compara una nave 500 creditos");
 
-        if(pincipal2.validarcompra(precio, saldo)==true){
+        if(pincipal2.validarcompra(precio, creditos)){
             System.out.println("la compra se ha realizado");
-            saldo=saldo-precio;
+            creditos-=precio;
         }
 
         else{
             System.out.println("la compra no se ha realizado, dinero insuficiente");
         }
-        System.out.println("el dinero que te queda es "+saldo);
+        System.out.println("el dinero que te queda es "+creditos+" creditos");
     }
 }
