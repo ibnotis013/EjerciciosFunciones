4ccb792d4a7e7f74d244daefa73e0c9936b991fd
ibnotis013 <n.ledo@liceolapaz.net>
2025-11-27 09:54:48 +0100

    ejercicio 5 terminado

--- src/ejercicio5/registrousuario.java
+++ src/ejercicio5/registrousuario.java
@@ -1,7 +1,29 @@
 package ejercicio5;
 
+import java.util.Scanner;
+
 public class registrousuario {
     static void main() {
+
+        Scanner sc = new Scanner(System.in);
+        String password;
+
+        System.out.println("ingrese el nombre de usuario");
+        String nombre = sc.nextLine();
+        System.out.println("ingrese un email");
+        String email = sc.nextLine();
+        do {
+            System.out.println("ingrese la contraseña");
+            password = sc.next();
+            if (validador.esPasswordSegura(password)==false) {
+                validador.mostrarErrores();
+            }
+
+        }while(validador.esPasswordSegura(password)==false);
+
+
+
+        System.out.println("Registro completado: Usuario: "+validador.formatearnombre(nombre)+" Registrado con email "+email);
 
     }
 }
--- src/ejercicio5/validador.java
+++ src/ejercicio5/validador.java
@@ -1,14 +1,43 @@
 package ejercicio5;
 
-public class validador {
-}
+import java.util.Locale;
 
-public static boolean esPasswordSegura(String pass){
-    if(pass.length()<8){
-        return true;
+public class validador {
+
+    public static boolean esPasswordSegura(String pass){
+        if(pass.length()>=8){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
-    else {
+
+    public static String formatearnombre(String nombre){
+        if(nombre==null || nombre.isEmpty()){
+            return nombre;
+        }
+        return nombre.substring(0, 1).toUpperCase() + nombre.substring(1);
+    }
+
+
+    public static boolean esEmailValido(String email){
+        if(email.contains("@")){
+            return true;
+        }
         return false;
     }
+
+
+    public static void mostrarErrores (){
+        System.out.println("la contraseña tiene que tener 8 o mas caracteres");
+    }
+
+
+
+
+
 }
+
+
 
