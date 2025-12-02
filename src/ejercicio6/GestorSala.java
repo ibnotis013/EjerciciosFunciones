23d70edcba961a88110e73ec0a08ffd72533d635
ibnotis013 <n.ledo@liceolapaz.net>
2025-12-02 08:45:09 +0100

    Ejercicio 6 terminado

--- src/ejercicio6/GestorSala.java
+++ src/ejercicio6/GestorSala.java
@@ -1,37 +1,44 @@
 package ejercicio6;
 
-public class gestorSala {
+public class GestorSala {
+
+
 
 
     public static void inicializarMatriz (char[][] sala){
         for(int i=0;i<sala.length;i++){
             for(int j=0;j<sala[i].length;j++){
-               sala[i][j]='L';
+                sala[i][j]='L';
             }
         }
     }
 
     public static void MostrarSala(char[][] sala){
         for(int i=0;i<sala.length;i++){
-            System.out.print(sala[i]);
             for(int j=0;j<sala[i].length;j++){
-                System.out.println(sala[i][j]);
+                System.out.print(sala[i][j]);
             }
+            System.out.println();
         }
 
     }
 
     public static boolean reservarAsiento (char[][] sala,int fila,int columna){
-        if(sala[fila][columna]>sala.length){
-        return false;
 
-        }
-        else if(sala[fila][columna]=='X'){
+        if(fila<0||fila>sala.length||columna>sala[0].length||columna<0){
+            System.out.println("error, asiento ocupado o coordenadas incorrectas");
             return false;
+        }
+      else if(sala[fila][columna]=='X'){
+          System.out.print("error, asiento ocupado o coordenadas incorrectas");
+          return false;
         } else if (sala[fila][columna]=='L') {
             sala[fila][columna]='X';
-            return true;
+          System.out.print("asiento reservado con exito");
+
+          return true;
         }
+        return  false;
     }
 
 
--- src/ejercicio6/TaquillaTeatro.java
+++ src/ejercicio6/TaquillaTeatro.java
@@ -1,10 +1,39 @@
 package ejercicio6;
 
-public class taquillaTeatro {
+import java.util.Scanner;
+
+public class TaquillaTeatro {
     static void main() {
-
+        Scanner sc = new Scanner(System.in);
+        String opcion;
         char[][] teatro = new char[5][5];
-        gestorSala.inicializarMatriz(teatro);
+        GestorSala.inicializarMatriz(teatro);
 
+
+
+        do {
+            System.out.println("seleccione una opcion\n" +
+                    "1 ver sala\n" +
+                    "2 reservar butaca\n" +
+                    "3 salir");
+            opcion = sc.next();
+            switch (opcion) {
+                case "1":
+                    GestorSala.MostrarSala(teatro);
+                    break;
+
+                case "2":
+                    System.out.println("introduce en que fila quieres reservar");
+                    int fila = sc.nextInt();
+                    System.out.println("introduce en que columna quieres reservar");
+                    int columna = sc.nextInt();
+                    GestorSala.reservarAsiento(teatro, fila, columna);
+                    break;
+
+            default:
+                System.out.println("opcion incorrecta");
+                break;
+        }
+        }while(!opcion.equals("3"));
     }
 }
